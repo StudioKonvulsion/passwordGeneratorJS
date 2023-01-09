914f6755a8795dff09c575f74cbe658a18913a67
Daniel <studiokonvulsion@gmail.com>
2023-01-09 11:14:38 +0000

    adding script

---
+++ script.js
@@ -0,0 +1,6 @@
+const lengthSlider = document.querySelector(".password-length input");
+const options = document.querySelectorAll(".option input");
+const copyIcon = document.querySelector(".input-box span");
+const passwordInput = document.querySelector(".input-box input");
+const passIndicator = document.querySelector(".password-indicator");
+const generateBtn = document.querySelector(".generate-btn");
